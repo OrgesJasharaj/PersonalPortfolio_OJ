b976a22b559188b948bade682bc30e8e604b35e6
OrgesJasharaj <109227463+OrgesJasharaj@users.noreply.github.com>
2022-07-14 14:17:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,22 @@
     <title>Personal Portfolio</title>
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+  
 </head>
 
 <body>
 
     <header>
         <div class="nav-bar">
-            <a href="" class="logo">Logo</a>
+            <a href="" class="logo">Orges Jasharaj</a>
             <div class="navigation">
                 <div class="nav-items">
                     <i class="uil uil-times nav-close-btn"></i>
                     <a href="#"><i class="uil uil-home"></i> Home</a>
-                    <a href="#"><i class="uil uil-compass"></i>About</a>
-                    <a href="#"><i class="uil uil-info-circle"></i>Skills</a>
-                    <a href="#"><i class="uil uil-document-layout-left"></i>Services</a>
+                    <a href="#"><i class="uil uil-user-circle"></i>About</a>
+                    <a href="#"><i class="uil uil-brackets-curly"></i>Skills</a>
+                    <a href="#"><i class="uil uil-database"></i>Services</a>
+                    
                     <a href="#"><i class="uil uil-envelope"></i>Contact</a>
                 </div>
             </div>
@@ -30,6 +32,35 @@
             <div class="scroll-indicator-bar"></div>
         </div>
     </header>
+
+ 
+
+
+    <script>
+    //Javacript for the scroll indicator bar
+    window.addEventListener("scroll", () => {
+      const indicatorBar = document.querySelector(".scroll-indicator-bar");
+
+      const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scrollValue = (pageScroll / height) * 100;
+
+      indicatorBar.style.width = scrollValue + "%";
+    });
+
+    //Responsive navigation menu toggle
+    const menuBtn = document.querySelector(".nav-menu-btn");
+    const closeBtn = document.querySelector(".nav-close-btn");
+    const navigation = document.querySelector(".navigation");
+
+    menuBtn.addEventListener("click", () => {
+      navigation.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      navigation.classList.remove("active");
+    });
+  </script>
 
 
 
