e50f69c51df4f739ca850cc67eff32cf00e9096a
OrgesJasharaj <109227463+OrgesJasharaj@users.noreply.github.com>
2022-07-16 21:08:31 +0200

    Trying something new...

--- index.php
+++ index.php
@@ -1,72 +1,19 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Portfolio</title>
-    <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    
-  
-</head>
-
-<body>
-
-    <header>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+  </head>
+      <header>
         <div class="nav-bar">
-            <a href="" class="logo">Orges Jasharaj</a>
-            <div class="navigation">
-                <div class="nav-items">
-                    <i class="uil uil-times nav-close-btn"></i>
-                    <a href="#"><i class="uil uil-home"></i> Home</a>
-                    <a href="#"><i class="uil uil-user-circle"></i>About</a>
-                    <a href="#"><i class="uil uil-brackets-curly"></i>Projects</a>
-                    <a href="#"><i class="uil uil-envelope"></i>Contact</a>
-                </div>
-            </div>
-            
-            <i class="uil uil-apps nav-menu-btn"></i>
+          <a href="#"></a>
         </div>
-        <div class="scroll-indicator-container">
-            <div class="scroll-indicator-bar"></div>
-        </div>
-    </header>
-
- 
-   
-  
-    <script>
-    //Javacript for the scroll indicator bar
-    window.addEventListener("scroll", () => {
-      const indicatorBar = document.querySelector(".scroll-indicator-bar");
-
-      const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
-      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const scrollValue = (pageScroll / height) * 100;
-
-      indicatorBar.style.width = scrollValue + "%";
-    });
-
-    //Responsive navigation menu toggle
-    const menuBtn = document.querySelector(".nav-menu-btn");
-    const closeBtn = document.querySelector(".nav-close-btn");
-    const navigation = document.querySelector(".navigation");
-
-    menuBtn.addEventListener("click", () => {
-      navigation.classList.add("active");
-    });
-
-    closeBtn.addEventListener("click", () => {
-      navigation.classList.remove("active");
-    });
-  </script>
- 
-  
+      </header>
 
 
+  </html>
 
-
-</body>
-
-</html>
